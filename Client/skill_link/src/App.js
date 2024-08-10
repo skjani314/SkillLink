5662db0c877a337fd60642f021d9ff868f7a8013
Shaik Mahammad jani <skskjani7@gmail.com>
2024-08-10 10:40:31 +0530

    environment variables added

--- Client/skill_link/src/App.js
+++ Client/skill_link/src/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'antd';
+import axios from 'axios';
 
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formData,setformData]=useState({name:'',email:'',password:'',phone:'',address:'',pincode:'',role:''});
+
+  axios.defaults.withCredentials=true;       
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -12,13 +16,40 @@ const App = () => {
     setIsModalOpen(false);
   };
 
+const handleSubmit=()=>
+{
+
+
+  axios.post('http://127.0.0.1:3300/register',formData)
+  .then(res=>console.log(res))
+  .catch(err=>console.log(err));
+
+
+}
+
+const handleformData=(e)=>{
+  const { name, value } = e.target;
+
+  setformData(prev => ({ ...prev, [name]: value }));
+}
+
+
   return (
     <>
       <Button type="primary" onClick={showModal}>
         Open Modal
       </Button>
-      <Modal title="Basic Modal" open={isModalOpen}  onCancel={handleCancel} footer={null}> 
-       
+      <Modal title="Basic Modal" open={isModalOpen} onCancel={handleCancel} footer={<Button type='primary' onClick={handleSubmit}>Submit</Button>}> 
+       <form>
+       <input type='text' name='name' placeholder='name' value={formData.name} onChange={handleformData} /><br></br>
+       <input type='email' name='email' placeholder='email'  value={formData.email} onChange={handleformData}/><br></br>
+       <input type='password' name='password' placeholder='password'  value={formData.password} onChange={handleformData}/><br></br>
+       <input type='text' name='phone' placeholder='phone'  value={formData.phone} onChange={handleformData} /><br></br>
+       <input type='text' name='address' placeholder='address'  value={formData.address} onChange={handleformData} /><br></br>
+       <input type='text' name='pincode' placeholder='pincode'  value={formData.pincode} onChange={handleformData}/><br></br>
+       <input type='text' name='role' placeholder='role'  value={formData.role} onChange={handleformData}/><br></br>
+
+       </form>
       </Modal>
     </>
   );
--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import axios from 'axios';
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
+import User from './models/User.js';
 
 
 dotenv.config();
 const app=express();
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
@@ -21,8 +19,32 @@ app.use(cors({
     allowedHeaders: 'Content-Type',
 })) 
 
-mongoose.connect('mongodb+srv://skskjani7:'+process.env.PASSWORD+'@users.3kyxw.mongodb.net/?retryWrites=true&w=majority&appName=Users',{
+mongoose.connect('mongodb+srv://skskjani7:'+process.env.PASSWORD+'@users.3kyxw.mongodb.net/SkillLink',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
+  app.post('/register',async (req,res)=>{
+    const {name,email,password,phone,address,pincode,role}=req.body;
+  
+    const user=await User.findOne({email});
+
+    if(user)
+      {
+        res.json("User Already Exist With Given Email");
+      }
+      else{
+       const hashpassword=await bcrypt.hash(password,10);
+
+      await User.create({name,email,password:hashpassword,role,phone,address,pincode})
+      .then(user=>res.json(user))
+      .catch(err=>res.json(err))
+  }
+    
+    });
+
+
+
+
+
+app.listen(process.env.PORT,()=>{console.log("server is running")});
