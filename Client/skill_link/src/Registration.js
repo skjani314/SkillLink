9119c8b4f2b9090e37e4bd93e8ab9059a95041be
GudipudiAkshaya <gudipudiakshaya22@gmail.com>
2024-08-23 15:20:43 +0530

    commited

---
+++ Client/skill_link/src/Registration.js
@@ -0,0 +1,9 @@
+export default function Registration(){
+    return(
+        <>
+          <div className="container border w-100 mt-5 m-auto p-4">
+            
+          </div>
+        </>
+    )
+}
